05edcd94a647988f270f1b858dbc6aefa53eb933
cucyzrd <292016178@qq.com>
2016-10-24 18:09:56 +0800

    get var from url

--- 5/app.py
+++ 5/app.py
@@ -3,12 +3,14 @@
 from flask import Flask
 import loganalysis
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/logs/')
 def logs():
     logfile = 'www_access_20140823.log'
     rt_list = loganalysis.get_topn(logfile=logfile)
+    print request.args
     return render_template('logs.html',rt_list=rt_list,title='top n log')
 
 @app.route('/')
