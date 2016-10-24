52097061332122cebaa7038c1f0ae923352544f4
cucyzrd <292016178@qq.com>
2016-10-24 15:03:42 +0800

    add top 10

--- 5/app.py
+++ 5/app.py
@@ -1,12 +1,19 @@
 #coding:utf-8
 
 from flask import Flask
-
+import loganalysis
 app = Flask(__name__)
+
+@app.route('/logs/')
+def logs():
+    logfile = 'www_access_20140823.log'
+    return loganalysis.loganalysis(logfile=logfile)
+
+
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return '<h1>hello world</h1>'
 
 if __name__ == '__main__':
     app.run(port=2000,debug=True)
